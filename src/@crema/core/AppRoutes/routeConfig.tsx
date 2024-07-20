4cb360bf61908fddca321a7886385d109c9c4fa3
RobertPham0327 <s3926681@rmit.edu.vn>
2024-07-20 10:10:52 +0700

    create main dahsboard page

--- src/@crema/core/AppRoutes/routeConfig.tsx
+++ src/@crema/core/AppRoutes/routeConfig.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import {BiAlignLeft} from 'react-icons/bi';
+import { PiStudentBold } from "react-icons/pi";
 
 const routesConfig = [
   {
     id: 'app',
-    title: 'Sample',
-    messageId: 'sidebar.sample',
+    title: 'Menu',
+    messageId: 'sidebar.navigation.menu',
     type: 'group',
     children: [
       {
-        id: 'page-1',
-        title: 'Page 1',
-        messageId: 'sidebar.sample.page1',
-        type: 'item',
-        icon: <BiAlignLeft />,
-        path: '/sample/page-1',
+        id: 'class_management',
+        title: 'Class management',
+        messageId: 'sidebar.classManagement',
+        type: 'collapse',
+        icon: <PiStudentBold />,
+        children: [
+          {
+            id: 'students',
+            title: 'Students',
+            messageId: 'sidebar.classManagement.students',
+            path: '/class-management/students',
+          },
+          {
+            id: 'teachers',
+            title: 'Teachers',
+            messageId: 'sidebar.classManagement.teachers',
+            path: '/class-management/teachers',
+          }
+        ]
       },
-      {
-        id: 'page-2',
-        title: 'Page 2',
-        messageId: 'sidebar.sample.page2',
-        type: 'item',
-        icon: <BiAlignLeft />,
-        path: '/sample/page-2',
-      },
+      // {
+      //   id: 'page-2',
+      //   title: 'Page 2',
+      //   messageId: 'sidebar.sample.page2',
+      //   type: 'item',
+      //   icon: <BiAlignLeft />,
+      //   path: '/sample/page-2',
+      // },
     ],
   },
 ];
--- src/modules/sample/Page1/index.tsx
+++ src/modules/sample/Page1/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import WelcomeCard from './WelcomeCard';
+import SalesState from './SalesState';
+
+
 
 const Page1 = () => {
-    return (
-      <>
-        <h2>Sample page 1</h2>
-        <div style={{ marginTop: 2 }}>You can kick start your app</div>
-      </>
-    );
+  return (
+    <>
+        <div style={{ marginBottom: "10px"}}>
+        <WelcomeCard data={[]}   />
+        </div>
+        <div style={{ marginTop: "30px"}}>
+        <SalesState salesState={[]} saleChartData={[]}   />
+        </div>
+    </>
+  );
 };
 
 export default Page1;
